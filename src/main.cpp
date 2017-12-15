42eb363424bfe00bb697a10c42a1801e8eb964dd
Oleksandra Ganus <ganus3636@gmail.com>
2017-12-15 23:00:20 +0100

    clean aup slam part;

--- src/main.cpp
+++ src/main.cpp
@@ -9,35 +9,19 @@
 
 // using namespace std;
 
-// void LoadImages(const string &strFile, vector<string> &vstrImageFilenames,
-//                 vector<double> &vTimestamps);
-
 // int main(int argc, char **argv)
 // {
-//     if(argc != 4)
+//     if(argc != 3)
 //     {
-//         cerr << endl << "Usage: ./mono_tum path_to_vocabulary path_to_settings path_to_sequence" << endl;
+//         cerr << endl << "Usage: " << argv[0] << " path_to_vocabulary path_to_settings path_to_sequence" << endl;
 //         return 1;
 //     }
 
-//     // Retrieve paths to images
-//     vector<string> vstrImageFilenames;
-//     vector<double> vTimestamps;
-//     string strFile = string(argv[3])+"/rgb.txt";
-//     LoadImages(strFile, vstrImageFilenames, vTimestamps);
-
-//     int nImages = vstrImageFilenames.size();
+//     const string strVocFile = argv[1];
+//     const string strSettingsFile = argv[2];
 
 //     // Create SLAM system. It initializes all system threads and gets ready to process frames.
-//     ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true);
-
-//     // Vector for tracking time statistics
-//     vector<float> vTimesTrack;
-//     vTimesTrack.resize(nImages);
-
-//     cout << endl << "-------" << endl;
-//     cout << "Start processing sequence ..." << endl;
-//     cout << "Images in the sequence: " << nImages << endl << endl;
+//     ORB_SLAM2::System SLAM(strVocFile,strSettingsFile,ORB_SLAM2::System::MONOCULAR,true);
 
 //     // create a video stream of the webcam
 //     cv::VideoCapture stream(1);
@@ -47,44 +31,16 @@
 
 //     // Main loop
 //     cv::Mat im;
-// //    int ni = 0; // image number
 //     while (true) {
 //         stream.read(im);
-//         cv::imshow("cam", im);
-//         cv::waitKey(30);
 //         double tframe = cv::getTickCount()/10000.0;
-
-// //        std::cout << "time = " << tframe << std::endl;
-
-// //#ifdef COMPILEDWITHC11
-// //        std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
-// //#else
-// //        std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
-// //#endif
 
 //         // Pass the image to the SLAM system
 //         SLAM.TrackMonocular(im,tframe);
-
-// //#ifdef COMPILEDWITHC11
-// //        std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
-// //#else
-// //        std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
-// //#endif
 //     }
 
 //     // Stop all threads
 //     SLAM.Shutdown();
-
-//     // Tracking time statistics
-// //    sort(vTimesTrack.begin(),vTimesTrack.end());
-// //    float totaltime = 0;
-// //    for(int ni=0; ni<nImages; ni++)
-// //    {
-// //        totaltime+=vTimesTrack[ni];
-// //    }
-// //    cout << "-------" << endl << endl;
-// //    cout << "median tracking time: " << vTimesTrack[nImages/2] << endl;
-// //    cout << "mean tracking time: " << totaltime/nImages << endl;
 
 //     // Save camera trajectory
 //     SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
@@ -92,40 +48,10 @@
 //     return 0;
 // }
 
-// void LoadImages(const string &strFile, vector<string> &vstrImageFilenames, vector<double> &vTimestamps)
-// {
-//     ifstream f;
-//     f.open(strFile.c_str());
-
-//     // skip first three lines
-//     string s0;
-//     getline(f,s0);
-//     getline(f,s0);
-//     getline(f,s0);
-
-//     while(!f.eof())
-//     {
-//         string s;
-//         getline(f,s);
-//         if(!s.empty())
-//         {
-//             stringstream ss;
-//             ss << s;
-//             double t;
-//             string sRGB;
-//             ss >> t;
-//             vTimestamps.push_back(t);
-//             ss >> sRGB;
-//             vstrImageFilenames.push_back(sRGB);
-//         }
-//     }
-// }
-
 
 #include <iostream>
 #include "calibration.h"
 #include "camera.h"
-
 
 int main(int argc, char **argv)
 {
